a566c5e6e131f36fd27232d4950e0c767b1e9c0d
pedro-mariotti <57444804+pedro-mariotti@users.noreply.github.com>
2019-12-09 13:23:33 -0300

    Atualizando o codigo de movimento
    Reformulei o codigo baseando-me no tutorial, para encaixar melhor com o sistema do hook.

--- PlayerMove.cs
+++ PlayerMove.cs
@@ -1,74 +1,99 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    public float jumpSpeed = 3f;
+    public bool groundCheck;
+    public bool isSwinging;
+    private SpriteRenderer playerSprite;
+    private Rigidbody2D rBody;
+    private bool isJumping;
+    private Animator animator;
+    private float jumpInput;
+    private float horizontalInput;
 
-    Animator MyAnimator;
-    SpriteRenderer MySprite;
-    Rigidbody2D body;
-    public float maxSpeed = 5f;
-    public float maxJump = 10f;
-    public bool ground;
-    private int direcao;
+    public Vector2 ropeHook;
+    public float swingForce = 4f;
 
-
-    // Use this for initialization
-    void Start ()
+    void Awake()
     {
-        body = this.GetComponent<Rigidbody2D>(); //traz o componente de rigidBody2D
-        MySprite = this.GetComponent<SpriteRenderer>();// traz o spriteRenderer
-        MyAnimator = this.GetComponent<Animator>();
-	}
+        playerSprite = GetComponent<SpriteRenderer>();
+        rBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
 
-
-    // Update is called once per frame
-    void Update ()
+    void Update()
     {
-
-        float move = Input.GetAxis("Horizontal"); //recebe o input
-        if (move < 0)
+        jumpInput = Input.GetAxis("Jump");
+        horizontalInput = Input.GetAxis("Horizontal");
+        var halfHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y;
+        groundCheck = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - halfHeight - 0.04f), Vector2.down, 0.025f);
+        if (groundCheck)
         {
-            direcao = -1;
+            animator.SetBool("grounded", true);
         }
-        else if (move > 0)
-        {
-            direcao = 1;
-        }
-
-       if (Input.GetButtonDown("Jump") && ground == true){ // detecta pulo
-            body.AddForce(Vector2.up * maxJump, ForceMode2D.Impulse);// adiciona força vertical no rigidBody
-                ground = false;
-                MyAnimator.SetBool("grounded", ground);
-        }
-
-        body.velocity = new Vector2(move * maxSpeed, body.velocity.y); // adiciona velocidade ao rigidbody2D
-
-        if (direcao == -1 && MySprite != null)
-        {
-            MySprite.flipX = true;
-        }
-        else if (direcao == 1)
-        {
-            MySprite.flipX = false;
-        }
-
- 
-            MyAnimator.SetFloat("velocityX", Mathf.Abs(body.velocity.x));
-
-	}
-
-    private void OnCollisionEnter2D(Collision2D collision2D)
-    {
-        if (collision2D.gameObject.tag == "Ground")
-        {
-            ground = true;
-            MyAnimator.SetBool("grounded", ground);
-        }
-	    
         else
         {
-            ground = false;
+            animator.SetBool("grounded", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (horizontalInput < 0f || horizontalInput > 0f)
+        {
+            animator.SetFloat("velocityX", Mathf.Abs(horizontalInput));
+            playerSprite.flipX = horizontalInput < 0f;
+            if (isSwinging)
+            {
+
+
+                var playerToHookDirection = (ropeHook - (Vector2)transform.position).normalized;
+
+   
+                Vector2 perpendicularDirection;
+                if (horizontalInput < 0)
+                {
+                    perpendicularDirection = new Vector2(-playerToHookDirection.y, playerToHookDirection.x);
+                    var leftPerpPos = (Vector2)transform.position - perpendicularDirection * -2f;
+                    Debug.DrawLine(transform.position, leftPerpPos, Color.green, 0f);
+                }
+                else
+                {
+                    perpendicularDirection = new Vector2(playerToHookDirection.y, -playerToHookDirection.x);
+                    var rightPerpPos = (Vector2)transform.position + perpendicularDirection * 2f;
+                    Debug.DrawLine(transform.position, rightPerpPos, Color.green, 0f);
+                }
+
+                var force = perpendicularDirection * swingForce;
+                rBody.AddForce(force, ForceMode2D.Force);
+            }
+            else
+            {
+                if (groundCheck)
+                {
+                    animator.SetBool("grounded", true);
+                    var groundForce = speed * 2f;
+                    rBody.AddForce(new Vector2((horizontalInput * groundForce - rBody.velocity.x) * groundForce, 0));
+                    rBody.velocity = new Vector2(rBody.velocity.x, rBody.velocity.y);
+                }
+            }
+        }
+        else
+        {
+            animator.SetFloat("velocityX", 0f);
+        }
+
+        if (!isSwinging)
+        {
+            if (!groundCheck) return;
+
+            isJumping = jumpInput > 0f;
+            if (isJumping)
+            {
+                rBody.velocity = new Vector2(rBody.velocity.x, jumpSpeed);//
+            }
         }
     }
 }
